717e382deab345fe1c070cd5effc4286c5baab88
유진 <youjin5587@gmail.com>
2022-12-09 20:26:21 +0900

    카테고리 가로정렬

--- src/category.js
+++ src/category.js
@@ -16,29 +16,30 @@ export default class MenuExampleSecondaryPointing extends Component {
         <Dropdown item text='카테고리' style={{fontSize: 17, marginLeft:50, marginRight:50}}>
         <Dropdown.Menu>
         <div style={{display: 'flex', justifyContent: 'center', width: '100%', padding: '20px'}}>        
-            <div>
-            <Dropdown.Header style={{fontSize: 20,  marginRight:100, marginLeft:20}}>드로잉</Dropdown.Header>
-            <Dropdown.Item>일러스트</Dropdown.Item>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>드로잉</Dropdown.Header>
+            <Link to="/searchpage"><Dropdown.Item>일러스트</Dropdown.Item></Link>
+
             <Dropdown.Item>인물 드로잉</Dropdown.Item>
             <Dropdown.Item>캘리그라피</Dropdown.Item>
             <Dropdown.Item>수채화</Dropdown.Item>
             <Dropdown.Item>디자인</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20, marginRight:100}}>공예</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>공예</Dropdown.Header>
             <Dropdown.Item>뜨개질</Dropdown.Item>
             <Dropdown.Item>비즈공예</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20,  marginRight:100}}>사진/영상</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20,  textAlign: 'center', marginBottom:'20px'}}>사진/영상</Dropdown.Header>
             <Dropdown.Item>사진</Dropdown.Item>
             <Dropdown.Item>영상</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20, marginRight:100}}>프로그래밍</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>프로그래밍</Dropdown.Header>
             <Dropdown.Item>IT교양</Dropdown.Item>
             <Dropdown.Item>프론트엔드</Dropdown.Item>
             <Dropdown.Item>백엔드</Dropdown.Item>
@@ -46,22 +47,22 @@ export default class MenuExampleSecondaryPointing extends Component {
             <Dropdown.Item>게임</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20, marginRight:100}}>음악</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>음악</Dropdown.Header>
             <Dropdown.Item>악기</Dropdown.Item>
             <Dropdown.Item>보컬/랩</Dropdown.Item>
             <Dropdown.Item>작곡</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20, marginRight:100}}>운동</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>운동</Dropdown.Header>
             <Dropdown.Item>스포츠</Dropdown.Item>
             <Dropdown.Item>피트니스</Dropdown.Item>
             <Dropdown.Item>스트레칭</Dropdown.Item>
             </div>
             
-            <div>
-            <Dropdown.Header style={{fontSize: 20, marginRight:100}}>외국어</Dropdown.Header>
+            <div style={{textAlign: 'center', marginRight:'30px'}}>
+            <Dropdown.Header style={{fontSize: 20, textAlign: 'center', marginBottom:'20px'}}>외국어</Dropdown.Header>
             <Dropdown.Item>영어</Dropdown.Item>
             <Dropdown.Item>외국어 시험</Dropdown.Item>
             <Dropdown.Item>제2외국어</Dropdown.Item>
